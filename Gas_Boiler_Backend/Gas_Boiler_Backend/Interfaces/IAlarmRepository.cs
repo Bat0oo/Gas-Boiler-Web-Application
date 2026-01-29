0fcc6a2909c403371a6c9463ba482e84a66633c1
Bat0oo <miloradmaksic01@gmail.com>
2026-01-30 00:11:51 +0100

    Repos for alarms

---
+++ Gas_Boiler_Backend/Gas_Boiler_Backend/Interfaces/IAlarmRepository.cs
@@ -0,0 +1,17 @@
+﻿using Gas_Boiler_Backend.DTO.Alarms;
+using Gas_Boiler_Backend.Models;
+
+namespace Gas_Boiler_Backend.Interfaces
+{
+    public interface IAlarmRepository
+    {
+        Task<Alarm> CreateAsync(Alarm alarm);
+        Task<Alarm?> GetByIdAsync(int id);
+        Task<IEnumerable<Alarm>> GetAllAsync(AlarmFiltersDto? filters = null);
+        Task<IEnumerable<Alarm>> GetActiveAlarmsAsync(int? userId = null);
+        Task<Alarm?> UpdateAsync(Alarm alarm);
+        Task<bool> DeleteAsync(int id);
+        Task<int> GetCountAsync(AlarmFiltersDto? filters = null);
+        Task<bool> ExistsRecentAlarmAsync(string type, int buildingId, int cooldownMinutes);
+    }
+}
