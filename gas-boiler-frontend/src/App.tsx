affcac0699f270f8e27f553d48dd627c34606dcd
Bat0oo <miloradmaksic01@gmail.com>
2026-02-24 23:14:22 +0100

    Added guest page

--- gas-boiler-frontend/src/App.tsx
+++ gas-boiler-frontend/src/App.tsx
@@ -7,18 +7,19 @@ import Register from './pages/Auth/Register';
 import Map from './pages/Map/Map';
 import ProtectedRoute from './components/ProtectedRoute';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import AppRoutes from './routes/AppRoutes'; 
+import AppRoutes from './routes/AppRoutes';
 import DashboardPage from './pages/DashboardPage/DashboardPage';
+import LandingPage from './pages/Landing/LandingPage';
 
 function App() {
    return (
     <AuthProvider>
       <Router>
         <Routes>
+          <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/*" element={<AppRoutes />} />
-          <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="dashboard" element={<DashboardPage />} />
         </Routes>
       </Router>
